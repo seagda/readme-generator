129f4d781866561c3f6756e1b4ffcddba64f195e
Shea Mullaney <seagda@gmail.com>
2021-03-02 02:21:42 -0800

    functions written

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const createMd = require("./scripts/createMarkdown");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { generate } = require("rxjs");
 
 // Create questions array
 
@@ -56,11 +57,32 @@ const questions = [
         name: "license",
         choices: [
             "Apache 2.0 License", 
-            "CC BY-SA Attribution-ShareAlike 4.0 International", "CC BY-NC Attribution-NonCommercial 4.0 International",
+            "CC BY-SA Attribution-ShareAlike 4.0 International", 
+            "CC BY-NC Attribution-NonCommercial 4.0 International",
             "GNU GPL v3", 
             "The MIT License", 
             "Mozilla Public License 2.0", 
             "Public Domain Dedication and License (PDDL)", 
             "The Artistic License 2.0"]
     }
-]
+];
+
+// write to the readme file
+
+function writeToFile(fileName, data) {
+    const contents = generate.createMd(data);
+    fs.appendFile(fileName, contents, (err) => {
+        err ? console.log(err) : console.log("Your readme has been generated, mate!")
+    })
+
+};
+
+// initialize the app 
+function init(){
+    inquirer.prompt(questions)
+            .then(response => {
+                if (!response) console.log("Error creating markdown.");
+                else writeToFile("./output/README.md", response);
+            })
+};
+init();
